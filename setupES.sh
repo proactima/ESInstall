4768971f47210ee28392cc524c4ba3a0a4b1723a
Anders Østhus <anders.osthus@outlook.com>
2014-03-05 08:58:00 +0000

    Added disk partition stuff

--- setupES.sh
+++ setupES.sh
@@ -4,21 +4,78 @@ export ES_VERSION=1.0.1
 export ES_AZURE_VERSION=2.0.0
 export ES_MARVEL_VERSION=1.0.2
 
-echo Installing VIM Config
-rm ~/.vimrc
-ln -s ~/ESInstall/ConfigFiles/vimrc ~/.vimrc
+function SetupVim {
+    echo Installing VIM Config
+    wget --no-check-certificate https://raw.github.com/proactima/ESInstall/master/ConfigFiles/vimrc
+    mv ~/vimrc ~/.vimrc
+}
 
-echo Updating system to latest packages
-sudo apt-get update
-sudo apt-get dist-upgrade -y
+function UpdateSystem {
+    echo Updating system to latest packages
+    sudo apt-get update
+    sudo apt-get dist-upgrade -y
+}
 
-echo Installing Java
-sudo apt-get install openjdk-7-jre-headless -y
+function InstallJava {
+    echo Installing Java
+    sudo apt-get install openjdk-7-jre-headless -y
+}
 
-echo Installing ElasticSearch
-curl -s https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-$ES_VERSION.deb -o elasticsearch-$ES_VERSION.deb
-sudo dpkg -i elasticsearch-$ES_VERSION.deb
+function InstallES {
+    echo Installing ElasticSearch
+    curl -s https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-$ES_VERSION.deb -o elasticsearch-$ES_VERSION.deb
+    sudo dpkg -i elasticsearch-$ES_VERSION.deb
 
-echo Installing ElasticSearch plugins
-sudo /usr/share/elasticsearch/bin/plugin -install elasticsearch/elasticsearch-cloud-azure/$ES_AZURE_VERSION
-sudo /usr/share/elasticsearch/bin/plugin -install elasticsearch/marvel/$ES_MARVEL_VERSION
+    echo Installing ElasticSearch plugins
+    sudo /usr/share/elasticsearch/bin/plugin -install elasticsearch/elasticsearch-cloud-azure/$ES_AZURE_VERSION
+    sudo /usr/share/elasticsearch/bin/plugin -install elasticsearch/marvel/$ES_MARVEL_VERSION
+}
+
+function ConfigureDataDisk {
+    esDataPath=/mnt/data
+
+    echo Checking for attached Windows Azure data disk
+    while [ ! -e /dev/sdc ]; do echo waiting for /dev/sdc empty disk to attach; sleep 20; done
+
+    
+}
+
+# Awesome ask function by @davejamesmiller https://gist.github.com/davejamesmiller/1965569
+function ask {
+    while true; do
+
+    if [ "${2:-}" = "Y" ]; then
+        prompt="Y/n"
+        default=Y
+    elif [ "${2:-}" = "N" ]; then
+        prompt="y/N"
+        default=N
+    else
+        prompt="y/n"
+        default=
+    fi
+
+    # Ask the question
+    read -p "$1 [$prompt] " REPLY
+
+    # Default?
+    if [ -z "$REPLY" ]; then
+        REPLY=$default
+    fi
+
+    # Check if the reply is valid
+    case "$REPLY" in
+        Y*|y*) return 0 ;;
+        N*|n*) return 1 ;;
+    esac
+
+    done
+}
+
+cd ~
+
+SetupVim
+#UpdateSystem
+#InstallJava
+#InstallES
+ConfigureDataDisk
